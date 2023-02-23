673f13c1fae6680a0f3e686303515222cc628a0e
jexline <joeyexline@gmail.com>
2023-02-23 11:21:49 -0800

    Add files via upload

---
+++ src/main/java/com/exline/craftobsidian/CraftObsidian.java
@@ -0,0 +1,12 @@
+package com.exline.craftobsidian;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CraftObsidian implements ModInitializer {
+
+    public static final String MOD_ID = "craftsobsidian";
+
+    @Override
+    public void onInitialize() {
+    }
+}
